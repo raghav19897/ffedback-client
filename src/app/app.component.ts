d418b9e30515834cddf3105fabe2e41eef039811
raghav <raghav19897@gmail.com>
2020-03-29 15:00:06 +0530

    Client app for feedback with google charts to show trend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Feedback-Client';
+
+  title1 = '';
+   type = 'LineChart';
+   data = [[0,0]];
+   columnNames = ['# Of Responses', 'Responses'];
+   options1 = {    
+   };
+   width = 550;
+   height = 400;
+
+  constructor(private httpClient: HttpClient){
+    this.httpClient.get("http://localhost:8080/GetAllData").subscribe(function(data){
+      for(let i=0; i<data.length; i++){
+        this.data.push([data[i].id, data[i].response])
+      }
+    }.bind(this));
+  }
+
+  sendResponse(){
+    let response = +(<HTMLInputElement>document.getElementById('response')).value/100;
+    this.httpClient.get("http://localhost:8080/SendResponse?response="+response).subscribe(data=>console.log(data));
+    location.reload()
+  }
 }
