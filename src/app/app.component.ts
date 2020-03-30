621ed10920b8d1061c954b690bacac886f756b52
raghav <raghav19897@gmail.com>
2020-03-31 01:51:06 +0530

    Updated app for fetching question, its options and send response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Feedback-Client';
+  question;
+  options;
 
-  title1 = '';
-   type = 'LineChart';
-   data = [[0,0]];
-   columnNames = ['# Of Responses', 'Responses'];
-   options1 = {    
-   };
-   width = 550;
-
-   height = 400;
   constructor(private httpClient: HttpClient){
-    this.httpClient.get("http://localhost:8080/GetAllData").subscribe(function(data){
-      this.data.pop();
-      for(let i=0; i<data.length; i++){
-        this.data.push([data[i].id, data[i].response])
-      }
-      console.log(this.data)
+    this.httpClient.get("http://localhost:8080/question").subscribe(function(data){
+      this.question = data;
+    }.bind(this));
+    this.httpClient.get("http://localhost:8080/options").subscribe(function(data){
+      this.options = data;
     }.bind(this));
   }
 
-  sendResponse(){
-    let response = +(<HTMLInputElement>document.getElementById('response')).value/100;
-    this.httpClient.post("http://localhost:8080/SendResponse?response="+response,{}).subscribe(data=>console.log(data));
-    location.reload()
+  sendResponse(id){
+    this.httpClient.get("http://localhost:8080/response?optionId="+id).subscribe(data=>console.log(data));
+    // location.reload()
   }
 }
